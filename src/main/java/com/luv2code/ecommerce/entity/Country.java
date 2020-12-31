050c593fb620f402ad09a5ad626867cb29d14618
Gabriel Bravo <gabriel.bravo@totemti.com.br>
2020-12-31 15:03:14 -0400

    country and states added

---
+++ src/main/java/com/luv2code/ecommerce/entity/Country.java
@@ -0,0 +1,29 @@
+package com.luv2code.ecommerce.entity;
+
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "country")
+@Data
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
+
+}
